cc22fad8d03af8f0b604a3233551788e5d29e873
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-08-28 07:23:50 -0400

    Implementação da classe Usuário

---
+++ ativ-6/Usuario.php
@@ -0,0 +1,25 @@
+<?php
+class Usuario {
+    private $id;
+    private $nome;
+    private $email;
+
+    public function __construct($id, $nome, $email) {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->email = $email;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+}
